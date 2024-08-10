6f7b1abf806950e6a1c31cdbf370367aa7ca610e
sarthakkathe009 <sarthakkathe1986@gmail.com>
2024-08-10 18:51:11 +0530

    added new models in model.py and admin.py

--- DjangoProject/project/models.py
+++ DjangoProject/project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class ChaiVariety(models.Model):
@@ -17,4 +18,35 @@ class ChaiVariety(models.Model):
     despriction=models.TextField(default="")
     def __str__(self):
         return self.name
+    
+# One to Many
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+
+#Many to Many
+class Store(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=50)
+    chai_varieties = models.ManyToManyField(ChaiVariety, related_name='store')
+
+    def __str__(self):
+        return self.name
+
+#One to One
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name="certificate")
+    certificate_number=models.CharField(max_length=100)
+    issue_date=models.DateTimeField(default=timezone.now)
+    valid_untill=models.DateField()
+    
+    def __str__(self):
+        return f'Certificate for {self.name.chai}'
+    
+
     
